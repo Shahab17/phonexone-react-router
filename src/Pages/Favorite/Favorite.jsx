b6923c86576178de8e11428e376a9fb57d3118b0
ShahabMat <cse_1812020090@lus.ac.bd>
2023-10-09 23:10:41 +0600

    price sumation added

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -8,11 +8,15 @@ const Favorite = () => {
     const [addFav, setAddFav] = useState([])
     const [noFound, setNoFound] = useState('')
     const [isShow, setIsShow] = useState(false)
+    const [totalPrice, setTotalPrice] = useState(0)
 
     useEffect(() => {
         const favItems = JSON.parse(localStorage.getItem('fav'))
         if (favItems) {
             setAddFav(favItems)
+
+            const total = favItems.reduce((preValue, currentValue) => preValue + currentValue.price, 0) // price jug korar jonno 
+            setTotalPrice(total.toFixed(2))
         } else {
             setNoFound('Not Found Your Product')
         }
@@ -38,6 +42,7 @@ const Favorite = () => {
                         {
                             addFav.length > 0 && <button onClick={handleFavRemove} className="btn block mx-auto bg-gradient-to-r from-cyan-50 to-blue-500 " >Delete All</button>
                         }
+                        <h2 className="text-xl font-medium"><span className="font-normal">Total:</span> ${totalPrice} </h2>
                         <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6">
                             {
                                 isShow ? addFav.map(phone => <Phone key={phone} phoneData={phone} ></Phone>)
@@ -47,8 +52,8 @@ const Favorite = () => {
                         </div>
 
                         {
-                            addFav.length > 3 && 
-                            <button onClick={() => setIsShow(!isShow)} className="btn block mt-6 mx-auto bg-gradient-to-r from-cyan-50 to-blue-500 " > {isShow ? 'See Less ' : 'See More'} </button>
+                            addFav.length > 3 &&
+                            <button onClick={() => setIsShow(!isShow)} className="btn block mt-6 mx-auto bg-gradient-to-r from-cyan-50 to-blue-500 " > {isShow ? 'See Less ' : 'See More'}  </button>
                         }
                     </div>
             }
